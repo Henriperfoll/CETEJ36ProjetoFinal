2b3ed10b6ad01aabc81b9b5cd0f7b16f264d7117
Henrique Perfoll Neto <hperfoll@gmail.com>
2019-08-17 18:19:48 -0300

    exemplo de menu iniciado

--- app/src/main/java/henriperfoll/cetej36projetofinal/MainActivity.java
+++ app/src/main/java/henriperfoll/cetej36projetofinal/MainActivity.java
@@ -2,6 +2,8 @@ package henriperfoll.cetej36projetofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.principal_opcoes, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuItemExcluir:
+
+                return true;
+
+            case R.id.menuItemSalvar:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+
     }
 }
